5d0d2b6bd62c409d354769fc705bdc0bdad2e610
Delta Student <mdmajidhussain1106@gmail.com>
2025-09-01 15:18:37 +0530

    fix: add missing newline and improve error handling in imageKit configuration feat: add comment for user data routes in userRouter

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,10 +1,14 @@
 import ImageKit from "imagekit";
 
+if (!process.env.IMAGEKIT_PUBLIC_KEY) {
+    console.log("Publickey is undefined")
+  throw new Error("Missing IMAGEKIT_PUBLIC_KEY");
+}
 
 var imagekit = new ImageKit({
     publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
+    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
 export default imagekit
--- server/routes/userRotes.js
+++ server/routes/userRotes.js
@@ -5,6 +5,7 @@ import { upload } from '../configs/multer.js';
 
 const userRouter = express.Router();
 
+// User data routes
 userRouter.get('/data',protect,  getUserData)
 userRouter.post('/update', upload.fields([{name: 'profile', maxCount: 1}, {name: 'cover', maxCount: 1}]), protect,  updateUserData)
 userRouter.post('/discover',protect,  discoverUsers)
