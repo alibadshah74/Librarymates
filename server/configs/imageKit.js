1ca874f950041b2a503a3cacbd52da47ecb7e241
Delta Student <mdmajidhussain1106@gmail.com>
2025-09-06 13:33:32 +0530

    fix: revert ImageKit configuration

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -2,9 +2,9 @@ import ImageKit from "imagekit";
 
 
 var imagekit = new ImageKit({
-    publicKey : "public_DduVZ9jTzTa5b+qnrznR5e0a3V8=",
-    privateKey : "private_6yMDveW8+neDG6lMrw3hxLyzezk=",
-    urlEndpoint : "https://ik.imagekit.io/1yb459mhs"
+    publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 
