e632754580f24b5ee117c913b471fcc76f1df81d
Delta Student <mdmajidhussain1106@gmail.com>
2025-09-06 15:23:32 +0530

    fix: wrap ImageKit instantiation in try-catch for error handling

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,11 +1,15 @@
 import ImageKit from "imagekit";
 
-
-var imagekit = new ImageKit({
+try {
+    var imagekit = new ImageKit({
     publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
+
 });
+} catch (error) {
+    console.log(error)
+}
 
 
 export default imagekit
